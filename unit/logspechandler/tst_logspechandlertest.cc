6664dfbd8f45f9a4f05ba3ca6ec005457c36b9a8
Domok Tamas <domoktamas@balabit.hu>
2013-11-04 11:36:39 +0100

    tests: logspechandler tests are more readable.

--- unit/logspechandler/tst_logspechandlertest.cc
+++ unit/logspechandler/tst_logspechandlertest.cc
@@ -46,30 +46,30 @@ LogspechandlerTest::cleanup()
 void
 LogspechandlerTest::test_invalid_pair()
 {
-  QVERIFY(logspec_handler->update_logsystem("a") == false);
-  QCOMPARE(LOG::LogSpecHandler::InvalidPair, logspec_handler->error());
+  QVERIFY(logspec_handler->update_logsystem(      "a") == false);
   QVERIFY(logspec_handler->errorString().contains("^"));
+  QCOMPARE(LOG::LogSpecHandler::InvalidPair, logspec_handler->error());
 
-  QVERIFY(logspec_handler->update_logsystem("a:b:c") == false);
-  QCOMPARE(LOG::LogSpecHandler::InvalidPair, logspec_handler->error());
+  QVERIFY(logspec_handler->update_logsystem(      "a:b:c") == false);
   QVERIFY(logspec_handler->errorString().contains("^"));
+  QCOMPARE(LOG::LogSpecHandler::InvalidPair, logspec_handler->error());
 }
 
 void
 LogspechandlerTest::test_invalid_log_level()
 {
-  QVERIFY(logspec_handler->update_logsystem("a:b") == false);
-  QCOMPARE(LOG::LogSpecHandler::InvalidLogLevel, logspec_handler->error());
+  QVERIFY(logspec_handler->update_logsystem(      "a:b") == false);
   QVERIFY(logspec_handler->errorString().contains("  ^"));
+  QCOMPARE(LOG::LogSpecHandler::InvalidLogLevel, logspec_handler->error());
 }
 
 void
 LogspechandlerTest::test_long_spec_error_string()
 {
-  QVERIFY(logspec_handler->update_logsystem("a:1;a:2;a:3;a:4;b:c;c:3") == false);
+  QVERIFY(logspec_handler->update_logsystem(      "a:1;a:2;a:3;a:4;b:c;c:3") == false);
+  QVERIFY(logspec_handler->errorString().contains("                  ^"));
+  QVERIFY(logspec_handler->update_logsystem(      "a:1;a:2;a:3;a:4;b:c:d;c:3") == false);
   QVERIFY(logspec_handler->errorString().contains("                ^"));
-  QVERIFY(logspec_handler->update_logsystem("a:1;a:2;a:3;a:4;b:c:d;c:3") == false);
-  QVERIFY(logspec_handler->errorString().contains("              ^"));
 }
 
 void
