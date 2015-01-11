0ca174fabf76fee6b2202e26fffdc078c1fb9fd7
Domok Tamas <tamas.domok@balabit.com>
2015-01-11 19:03:31 +0100

    project: added copyright for source files.

--- qlogsystem/standardlogformatter.hh
+++ qlogsystem/standardlogformatter.hh
@@ -1,3 +1,22 @@
+/**********************************************************************************
+ * Copyright (c) 2013-2015 BalaBit IT Ltd, Budapest, Hungary
+ *
+ * This file is part of qlogsystem.
+ *
+ * qlogsystem is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * qlogsystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with qlogsystem; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **********************************************************************************/
+
 #ifndef QLOGSYSTEM_STANDARDLOGFORMATTER_HH
 #define QLOGSYSTEM_STANDARDLOGFORMATTER_HH
 
